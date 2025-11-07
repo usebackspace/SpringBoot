cbc9ca28479948daf2c59c1f51ffb796bc37aa45
Sandip Jaiswar <jaiswarsandeep00@gmail.com>
2025-11-07 15:42:30 +0530

    up

---
+++ template_inheritance/src/main/java/com/example/first_project/controller/PageController.java
@@ -0,0 +1,23 @@
+package com.example.first_project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+    
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/about")
+    public String about(){
+        return "about";
+    }
+
+    @GetMapping("/contact")
+    public String contact(){
+        return "contact";
+    }
+}
