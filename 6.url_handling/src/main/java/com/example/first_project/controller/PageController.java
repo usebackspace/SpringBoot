843265d5bef0c116c33824c0abcf7944f9c1c352
Sandip Jaiswar <jaiswarsandeep00@gmail.com>
2025-11-10 12:23:43 +0530

    url-handling added

---
+++ 6.url_handling/src/main/java/com/example/first_project/controller/PageController.java
@@ -0,0 +1,27 @@
+package com.example.first_project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class PageController {
+    
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    //  @GetMapping("/product")
+    // public String product(Model model){
+    //     model.addAttribute("product_id", 100);
+    //     return "product";
+
+    @GetMapping("/product/{id}")
+    public String product(@PathVariable int id, Model model){
+        model.addAttribute("product_id", id);
+        return "product";
+    }
+}
